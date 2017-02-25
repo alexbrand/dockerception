e133eb86aecc03bb2c4e807afb19709fac5f64cd
Alexander Brand <abrand@apprenda.com>
2017-02-25 11:14:40 -0500

    some ascii art and call docker version

--- main.go
+++ main.go
@@ -6,9 +6,22 @@ import (
 	"os/exec"
 )
 
+// Echo this from within the a container
+const dc = `
+______               _                                    _    _               
+|  _  \             | |                                  | |  (_)              
+| | | |  ___    ___ | | __  ___  _ __   ___   ___  _ __  | |_  _   ___   _ __  
+| | | | / _ \  / __|| |/ / / _ \| '__| / __| / _ \| '_ \ | __|| | / _ \ | '_ \ 
+| |/ / | (_) || (__ |   < |  __/| |   | (__ |  __/| |_) || |_ | || (_) || | | |
+|___/   \___/  \___||_|\_\ \___||_|    \___| \___|| .__/  \__||_| \___/ |_| |_|
+                                                  | |                          
+						                                                                             
+`
+
 func main() {
+	runOrQuit("docker", "version")
 	runOrQuit("docker", "pull", "busybox")
-	runOrQuit("docker", "run", "busybox", "echo", "Dockerception!")
+	runOrQuit("docker", "run", "busybox", "echo", dc)
 	runOrQuit("docker", "ps", "-a")
 }
 
