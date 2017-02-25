0b90959c5c6885661652c90ac1a51570455fff88
Alexander Brand <abrand@apprenda.com>
2017-02-24 20:37:10 -0500

    Create and start a container

--- main.go
+++ main.go
@@ -6,15 +6,33 @@ import (
 	"os"
 
 	"github.com/docker/engine-api/client"
+	"github.com/docker/engine-api/types"
+	"github.com/docker/engine-api/types/container"
+	"github.com/docker/engine-api/types/strslice"
 )
 
 func main() {
+	// Create docker client
 	cli, err := client.NewEnvClient()
 	quitOnErr(err)
+
+	// Let's see if we can even talk to it
 	ver, err := cli.ServerVersion(context.Background())
 	quitOnErr(err)
 	fmt.Println("Was able to connect to docker daemon")
 	fmt.Println(ver)
+
+	// Let's create a container
+	cmd := strslice.StrSlice{"echo", "hello", "world"}
+	containerConf := &container.Config{
+		Image: "busybox",
+		Cmd:   cmd,
+	}
+	resp, err := cli.ContainerCreate(context.Background(), containerConf, nil, nil, "")
+	quitOnErr(err)
+	err = cli.ContainerStart(context.Background(), resp.ID, types.ContainerStartOptions{})
+	quitOnErr(err)
+	fmt.Println("Was able to run a container")
 }
 
 func quitOnErr(err error) {
