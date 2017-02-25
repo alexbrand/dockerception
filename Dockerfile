750f48b173f6f661e602d1f059d879cebbefca2a
Alexander Brand <abrand@apprenda.com>
2017-02-24 21:13:14 -0500

    Download docker CLI and use it instead of library

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
 FROM golang:1.8.0
 
+RUN curl https://get.docker.com/builds/Linux/x86_64/docker-1.12.2.tgz -O && tar -xf docker-1.12.2.tgz
+
+RUN mv docker/docker /usr/bin/docker
+
 ADD . /go/src/github.com/alexbrand/dockerception
--- main.go
+++ main.go
@@ -1,40 +1,17 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
-
-	"github.com/docker/engine-api/client"
-	"github.com/docker/engine-api/types"
-	"github.com/docker/engine-api/types/container"
-	"github.com/docker/engine-api/types/strslice"
+	"os/exec"
 )
 
 func main() {
-	// Create docker client
-	cli, err := client.NewEnvClient()
+	cmd := exec.Command("docker", "run", "busybox", "echo", "Dockerception!")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
 	quitOnErr(err)
-
-	// Let's see if we can even talk to Docker daemon
-	ver, err := cli.ServerVersion(context.Background())
-	quitOnErr(err)
-	fmt.Println("Was able to connect to docker daemon")
-	fmt.Println(ver)
-
-	// Let's create a container
-	cmd := strslice.StrSlice{"echo", "hello", "world"}
-	containerConf := &container.Config{
-		Image: "busybox",
-		Cmd:   cmd,
-	}
-	resp, err := cli.ContainerCreate(context.Background(), containerConf, nil, nil, "")
-	quitOnErr(err)
-
-	// Let's start the container
-	err = cli.ContainerStart(context.Background(), resp.ID, types.ContainerStartOptions{})
-	quitOnErr(err)
-	fmt.Println("Was able to run a container")
 }
 
 func quitOnErr(err error) {
