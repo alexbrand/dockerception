49a48a4de21bac7e801b9147b9dfd95732db7550
Alexander Brand <abrand@apprenda.com>
2017-02-26 10:21:59 -0500

    Use host network namespace

--- nested-docker/test.sh
+++ nested-docker/test.sh
@@ -2,7 +2,8 @@
 
 docker build . -t nested-docker
 
-CONTAINER_ID=$(docker run --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -d nested-docker)
+CONTAINER_ID=$(docker run --net host --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -d nested-docker)
+echo $CONTAINER_ID
 
 # Wait for docker daemon to start up
 sleep 3 
@@ -11,4 +12,4 @@ docker exec $CONTAINER_ID docker run busybox echo nested-docker!
 
 docker exec $CONTAINER_ID bash -c 'echo 1 > /sys/module/nf_conntrack/parameters/hashsize'
 
-#docker kill $CONTAINER_ID
+docker kill $CONTAINER_ID
