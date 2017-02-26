f69e85ba7957b8436b054231493382c81369a8fc
Alexander Brand <abrand@apprenda.com>
2017-02-26 10:11:50 -0500

    Test writing to sys

--- nested-docker/test.sh
+++ nested-docker/test.sh
@@ -8,3 +8,7 @@ CONTAINER_ID=$(docker run --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -d ne
 sleep 3 
 
 docker exec $CONTAINER_ID docker run busybox echo nested-docker!
+
+docker exec $CONTAINER_ID bash -c 'echo 1 > /sys/module/nf_conntrack/parameters/hashsize'
+
+#docker kill $CONTAINER_ID
